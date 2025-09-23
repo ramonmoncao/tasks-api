4f60161a51b0dee956f289e63070316de7fd81f4
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-23 11:03:32 -0300

    Criação da entidade task

---
+++ src/main/java/com/ramonmoncao/tasks_api/domain/model/Task.java
@@ -0,0 +1,30 @@
+package com.ramonmoncao.tasks_api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    @Column(nullable = false)
+    private boolean done;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+}
