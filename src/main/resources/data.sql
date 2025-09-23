4696e4ae5744a29e13a2ff77823395280cea95d0
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-23 13:52:21 -0300

    População do banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tasks (title, done) VALUES ('Estudar Spring Boot', FALSE);
+INSERT INTO tasks (title, done) VALUES ('Fazer exercício de SQL', TRUE);
+INSERT INTO tasks (title, done) VALUES ('Implementar API de tarefas', FALSE);
+INSERT INTO tasks (title, done) VALUES ('Testar integração com H2', FALSE);
