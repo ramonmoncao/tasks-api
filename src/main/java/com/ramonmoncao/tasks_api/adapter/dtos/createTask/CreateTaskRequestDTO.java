5c044deae9e2448cc01f2ce02d57eb2a39c75aa1
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-24 10:05:47 -0300

    Rollback Pasta Adapter

---
+++ src/main/java/com/ramonmoncao/tasks_api/adapter/dtos/createTask/CreateTaskRequestDTO.java
@@ -0,0 +1,19 @@
+package com.ramonmoncao.tasks_api.adapter.dtos.createTask;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateTaskRequestDTO {
+    private String title;
+    private boolean done;
+    private LocalDateTime createdAt;
+}
